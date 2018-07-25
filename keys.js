42a29010c900b2a8562977eacd24dc5be124a530
Anuhya Sai Nudurupati <anuhya.sai28@gmail.com>
2018-07-25 09:46:59 -0700

    keys

--- keys.js
+++ keys.js
@@ -1,19 +1,18 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: "tH6xunSqdtLR0IHmtiqJ6gfjc",
-  consumer_secret: "9XtODAmISzHYMOWanDzP5ZiGXURHfLg4d3wzlAFa1sElWcAQ5R",
-  access_token_key: "1015048258501029888-9nZ3Oxz5ZKm19c2oJ6fMP5eY4Ezv1v",
-  access_token_secret: "qLpX22pI6TcH4eofunIJdjmgy5a9uMJv393MDHFcVG0FO"
+  consumer_key: TWITTER_CONSUMER_KEY,
+  consumer_secret: TWITTER_CONSUMER_SECRET,
+  access_token_key: TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: "e4b502e8cd81498a9e6cb66fd417c79d",
-  secret: "fceb6d24180044079c077a1e26a7e19c"
+  id: SPOTIFY_ID,
+  secret: SPOTIFY_SECRET
 };
 
-//OMDb API: https://www.omdbapi.com/?i=tt3896198&apikey=2974992a
 exports.omdb = {
-  i: "tt3896198",
-  apikey: "2974992a"
+  i: OMDB_ID,
+  apikey: OMDB_APIKEY
 };
